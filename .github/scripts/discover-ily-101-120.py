1e281c0c8869e9238d369af640e7f02ceb8203b5
Adisabeba33 <lezzgo2@gmail.com>
2026-08-29 15:04:42 -0400

    chore: remove obsolete ILY discovery script

--- .github/scripts/discover-ily-101-120.py
+++
@@ -1,9 +0,0 @@
-import urllib.request,re,html
-urls=['https://furryfriendsnutrition.com/products/i-and-love-and-you-stir-mix-a-little-chicken-bone-broth-instant-home-made-dog-food','https://furryfriendsnutrition.com/products/i-and-love-and-you-stir-mix-a-little-turkey-bone-broth-instant-home-made-dog-food','https://furryfriendsnutrition.com/products/i-and-love-and-you-stir-mix-a-little-beef-bone-broth-instant-home-made-dog-food']
-for u in urls:
-  try:
-    req=urllib.request.Request(u,headers={'User-Agent':'Mozilla/5.0'})
-    s=urllib.request.urlopen(req,timeout=30).read().decode(errors='ignore')
-    s=re.sub(r'<script.*?</script>|<style.*?</style>',' ',s,flags=re.S|re.I);s=re.sub(r'<[^>]+>',' ',s);s=html.unescape(re.sub(r'\s+',' ',s))
-    print('URL',u);print(s[:12000])
-  except Exception as e:print('FAIL',u,e)
